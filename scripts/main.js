0d3dbfcd8f4ae57612f5ad93c22f0bcb0282158d
Giveonaldo <giveonaldo@gmail.com>
2023-03-25 09:15:37 +0700

    add

--- scripts/main.js
+++ scripts/main.js
@@ -13,22 +13,24 @@ let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
 function setUserName() {
-    const myName = prompt("Please enter your name.");
-    if (!myName){
-        setUserName();
-    } else {
-        localStorage.setItem("name", myName);
+  const myName = prompt("Please enter your name :");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
     myHeading.textContent = `Mozilla is cool, ${myName}`;
-    }
+  }
 }
 
 if (!localStorage.getItem("name")) {
-    setUserName();
+  setUserName();
 } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
 }
 
-myButton.onclick = () => {
-    setUserName();
-};
+// myButton.onclick = () => {
+//     setUserName();
+// };
+
+myButton.addEventListener("click", setUserName);
